dd95de68b33d3b769af571c376f8112d9e8141a6
ianmmworia <ianmworia156@gmail.com>
2024-11-25 13:53:24 +0300

    Added Contact Page transitions

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,30 @@
 import Woman from "../img/contact/woman.png";
-import Crew from "../img/contact/Behind the Scenes 2.jpg"
+import Crew from "../img/contact/Behind_the_Scenes_2-removebg-preview.png";
+
+import { motion } from "framer-motion";
+
+import { transition1 } from "../transitions";
 
 const Contact = () => {
   return (
-    <section className="section bg-white">
+    <motion.section
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+      className="section bg-white"
+    >
       <div className="container mx-auto h-full">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left">
-          <div className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10">bg</div>
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={transition1}
+            className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10"
+          >
+            bg
+          </motion.div>
           <div className="lg:flex-1 lg:pt-32 px-4">
             <h1 className="h1">Contact Us</h1>
             <p className="mb-12">We would love to hear from you</p>
@@ -29,15 +47,23 @@ const Contact = () => {
                 type="email"
                 placeholder="Your Message"
               />
-              <button className="btn mb-[30px] mx-auto lg:mx-0 self-start">Send It</button>
+              <button className="btn mb-[30px] mx-auto lg:mx-0 self-start">
+                Send It
+              </button>
             </form>
           </div>
-          <div className="lg:flex-1">
-            <img src={Crew} alt=""/>
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={{ transition: transition1, duration: 1.5 }}
+            className="lg:flex-1"
+          >
+            <img src={Crew} alt="" />
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
