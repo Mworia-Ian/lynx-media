20a7cbbeba3fe50254f6e21d7183f739c8aec7e9
ianmmworia <ianmworia156@gmail.com>
2024-11-25 13:10:10 +0300

    Added about us page transitions

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,26 +4,38 @@ import WomanImg from "../img/about/woman.png";
 import Crew from "../img/about/crew.jpg";
 import dance from "../img/about/dance.jpg";
 
-
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const About = () => {
   return (
-    <section className="section">
+    <motion.section
+      initial={{ opacity: 0, y: '100%' }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: '100%' }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
           <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden mt-11">
             <img src={dance} alt="" />
           </div>
-          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start mt-80">
+          <motion.div  
+          initial={{ opacity: 0, y: '-80%' }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: '-80%' }}
+          transition={transition1}
+          className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start mt-80">
             <h1 className="h1">About Us</h1>
             <p className="mb-10 max-w-sm">
-              Mambo Stranger😃.…we are a bunch of adults who play
-              with cameras, lenses, and light to create beauty! Sometimes we ask
+              Mambo Stranger😃.…we are a bunch of adults who play with cameras,
+              lenses, and light to create beauty! Sometimes we ask
               respected,responsible adults to show us their teeth to prove that
               they are happy but instead, they cry. Tears of joy. Tears of
-              Sadness. We dont mind tears or smiles. Our superpower is to
-              freeze them in time. And turn strangers into friends.
+              Sadness. We dont mind tears or smiles. Our superpower is to freeze
+              them in time. And turn strangers into friends.
               <br />
               <br />
               Sometimes the corporate world calls us to order, then we become a
@@ -38,10 +50,10 @@ const About = () => {
             <Link to={"/portfolio"} className="btn ">
               View Our Work
             </Link>
-          </div>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
